e10c0f90ed3412150a1ca7cece8a2b3ee5f15ce1
Ertan Turan <ertanturan@outlook.com>
2019-12-19 10:41:47 +0300

    Package Imported : HoloLensWithOpenCVForUnityExample_v1.0.2

---
+++ Assets/HoloLensWithOpenCVForUnityExample/ShowLicense.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace HoloLensWithOpenCVForUnityExample
+{
+    public class ShowLicense : ExampleSceneBase
+    {
+        // Use this for initialization
+        protected override void Start ()
+        {
+            base.Start ();
+        }
+        
+        // Update is called once per frame
+        void Update ()
+        {
+            
+        }
+
+        /// <summary>
+        /// Raises the back button click event.
+        /// </summary>
+        public void OnBackButtonClick ()
+        {
+            LoadScene ("HoloLensWithOpenCVForUnityExample");
+        }
+    }
+}
